0a03adefb7f9d4e7043e42dcea287016d825fc9d
Josh Ellsworth <jellsworth@xpanxion.com>
2021-04-19 16:05:53 -0400

    customer repository and model

---
+++ PremoAlDente-main/src/main/java/Premo/PremoAlDente/Repositories/CustomerRepository.java
@@ -0,0 +1,11 @@
+//package Premo.PremoAlDente.Repositories;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import Premo.PremoAlDente.Models.Customer;
+//
+//public interface CustomerRepository extends JpaRepository<Customer, Long> {
+//	List<Customer> findByPhoneNumberContaining(String phoneNumber);
+//}
