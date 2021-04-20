95461bf2a9b740f1d84b776021a4dc0e01be3fc7
rni <rni@xpanxion.com>
2021-04-20 17:02:26 -0400

    modified Order Controller/Order

--- src/main/java/Premo/PremoAlDente/Controllers/OrderController.java
+++ src/main/java/Premo/PremoAlDente/Controllers/OrderController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import Premo.PremoAlDente.Models.Order;
 import Premo.PremoAlDente.Repositories.OrderRepository;
 
-@Controller
 @CrossOrigin(origins = "http://localhost:4200")
+@RestController
 @RequestMapping("/api")
 public class OrderController {
 	
--- src/main/java/Premo/PremoAlDente/Repositories/OrderRepository.java
+++ src/main/java/Premo/PremoAlDente/Repositories/OrderRepository.java
@@ -1,8 +1,12 @@
 package Premo.PremoAlDente.Repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import Premo.PremoAlDente.Models.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
+	List<Order> findByTimestampContaining(Date timestamp);
 }
